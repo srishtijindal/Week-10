b3d1b02dab3b9bb3a095d8457272f705527fc43a
srishtijindal <srishti.jindal@my.jcu.edu.au>
2016-05-24 10:28:49 +0530

    test

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
-print("hello")
+#create a new type of exception
+class Low_Inventory_Exception(Exception):
+    def __init__(self):
+        print("The inventory is low!")
+
+def test():
+    inventory = 11
+    if inventory < 10:
+        raise Low_Inventory_Exception
+
+try:
+    test()
+except(Low_Inventory_Exception):
+    print("Something is not right.")
+else:
+    print("Everything is good.")
+finally:
+    print("Everything is fixed.")
